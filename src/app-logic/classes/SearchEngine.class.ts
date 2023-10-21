4aa1336638ae628d45cea38c54e28dd78e5d6a7d
Brian Joseph <bjosephm.93@gmail.com>
2023-10-22 01:25:07 +0200

    fixed bug with SystemFile Interface

--- src/app-logic/classes/SearchEngine.class.ts
+++ src/app-logic/classes/SearchEngine.class.ts
@@ -3,14 +3,6 @@ import { FileSystemSearch } from "./FileSystemSearch.class"
 import { SearchError } from "../../../types/SearchError"
 import { SystemFile } from "../../../types/SystemFile"
 
-interface FoundFile {
-    name: string,
-    uri: string,
-    perfectMatch: boolean,
-    size: number,
-}
-
-
 export class SearchEngine {
     private fsSearch: FileSystemSearch
     constructor() {
--- types/SystemFile.ts
+++ types/SystemFile.ts
@@ -1,3 +1,7 @@
 export interface SystemFile {
-
+    name: string,
+    originalUri?: string
+    uri?: string,
+    perfectMatch?: boolean,
+    size?: number,
 }
