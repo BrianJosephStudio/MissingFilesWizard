a81317c9ab8eae1696f6c0f83927eb6e71a89a6e
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-22 10:19:22 +0200

    Created SearchResults class

---
+++ src/app-logic/classes/SearchResults.class.ts
@@ -0,0 +1,30 @@
+import { SystemFile } from "@root/types/SystemFile"
+import { ResultsRegistry } from "@classes/ResultsRegistry.class"
+import { v4 as uuidv4 } from "uuid"
+
+export class SearchResults {
+    public readonly id: string
+    private results: SystemFile[]
+    private locked: boolean
+    private readonly errorMessage: string = "Instance is locked."
+    constructor() {
+        this.locked = false
+        this.id = uuidv4()
+        this.results = []
+    }
+    public add(file: SystemFile): void {
+        if (this.locked) { throw new Error(this.errorMessage) }
+        this.results.push(file)
+    }
+    public get(): SystemFile[] {
+        return this.results
+    }
+    public addToRegistry() {
+        if (this.locked) { throw new Error(this.errorMessage) }
+        ResultsRegistry.getInstance().addSearchResults(this)
+        this.lockInstance()
+    }
+    private lockInstance() {
+        this.locked = true
+    }
+}
