c6153e23210d036bfb200886c53d535930f973f6
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-22 10:15:41 +0200

    Added Path Aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@root": "/",
+      "@types": "/types",
+      "@classes": "/src/app-logic/classes",
+      "@components": "/src/components",
+      "@views": "/src/views"
+    }
+  }
 })
