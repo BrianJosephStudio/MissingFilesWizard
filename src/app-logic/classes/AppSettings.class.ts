2c47a0349218c76e680df7b826bb213aa68149b0
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-25 23:42:19 +0200

    Created AppSettings class

---
+++ src/app-logic/classes/AppSettings.class.ts
@@ -0,0 +1,18 @@
+interface Settings {
+
+}
+export default class AppSettings {
+    constructor() {
+
+    }
+
+    public async changeSetting(): Promise<Settings> {
+
+
+        return {}
+    }
+
+    public async fetchSettings(): Promise<Settings> {
+        return {}
+    }
+}
