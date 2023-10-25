83dc7c5107bb78593074d9123ebe2fefae820449
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-25 23:42:56 +0200

    Created Main.Page.vue wip

---
+++ src/pages/Main.Page.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container" :data-page="Pages.MAIN">
+        <button @click="extendScript.relinkMissingFiles">
+            Relink
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Pages } from "../../types/Pages.d"
+import ExtendScriptAPI from "@classes/ExtendScriptAPI.class";
+// import { onMounted } from "vue/dist/vue.js";
+
+const extendScript = new ExtendScriptAPI()
+
+</script>
+<style lang="scss"></style>
