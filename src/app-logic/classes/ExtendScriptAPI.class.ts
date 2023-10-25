d089950e2cd130c564e8c09896a7fd5110378ddc
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-25 23:42:30 +0200

    Created EstendScriptAPI

---
+++ src/app-logic/classes/ExtendScriptAPI.class.ts
@@ -0,0 +1,29 @@
+import AppSettings from "@classes/AppSettings.class";
+import { CSInterface } from "csinterface-ts"
+
+export enum SCRIPTS {
+
+}
+
+export default class ExtendScriptAPI {
+    private settings: AppSettings
+    private cs?: CSInterface
+
+    constructor() {
+        this.settings = new AppSettings()
+        try {
+            this.cs = new CSInterface()
+        } catch (e) {
+
+        }
+
+    }
+    public async relinkMissingFiles(): Promise<void> {
+        // const settings = await this.settings.fetchSettings()
+        // const serializedSettings = JSON.stringify(settings)
+
+        // this.cs.evalScript(`relinkMissingFiles(${serializedSettings})`, () => { })
+
+        console.log("works")
+    }
+}
