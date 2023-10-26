762cbbb9a8029f742305b9f6a3866fdedb95c4e6
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-26 11:07:38 +0200

    Created DropDownComponent

---
+++ src/components/DropDown.vue
@@ -0,0 +1,13 @@
+<template>
+    <select :id="Id" :name="Name">
+        <option v-for="option of Options" :value="option">{{ option }}</option>
+    </select>
+</template>
+<script setup lang="ts">
+
+const props = defineProps({
+    Id: String,
+    Name: String,
+    Options: Array
+})
+</script>
