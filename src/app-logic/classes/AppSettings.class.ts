df9ee626bcbb60d5e3023e8542a993e1e68fb04f
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-26 11:07:02 +0200

    Added new methods to AppSettings class

--- src/app-logic/classes/AppSettings.class.ts
+++ src/app-logic/classes/AppSettings.class.ts
@@ -1,18 +1,29 @@
-interface Settings {
-
-}
+// import { readFile } from "fs/promises"
+import config from "../../../config"
+import { Settings, MISSINGFILESPOOL, SEARCHPOOL, RELINKMODE } from "@root/types/Settings.d"
 export default class AppSettings {
     constructor() {
 
     }
 
-    public async changeSetting(): Promise<Settings> {
+    public async changeSetting(): Promise<any> {
 
-
+        await this.getHomeDir()
         return {}
     }
 
     public async fetchSettings(): Promise<Settings> {
-        return {}
+        // const settingsString = await readFile(config.settingsPath, { encoding: "utf-8" })
+        // const settings = JSON.parse(settingsString) as Settings
+        // return settings
+
+        return config.defaultSettings
+
+    }
+    private async getHomeDir(): Promise<string> {
+        return "HomeDirTest"
+    }
+    public async getProjectDir(): Promise<string> {
+        return "ProjectDirTest"
     }
 }
