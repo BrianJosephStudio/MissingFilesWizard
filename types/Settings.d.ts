5ec2f50ed53ea1247b543f61e7bad1a8611f3a54
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-27 11:03:06 +0200

    refactored settings keys for readability

--- types/Settings.d.ts
+++ types/Settings.d.ts
@@ -1,20 +1,29 @@
-export enum RELINKMODE {
-    RELINKMISSING
+export enum RELINKMETHOD {
+    RECONNECT,
+    REPLACE
 }
+export const relinkMethod = Object.keys(RELINKMETHOD).slice(Object.keys(RELINKMETHOD).length / 2).map(value => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase())
+
 export enum MISSINGFILESPOOL {
-    PROJECT
+    SELECTION,
+    PROJECT,
+    ACTIVECOMP,
 }
+export const missingFilesPool = Object.keys(MISSINGFILESPOOL).slice(Object.keys(MISSINGFILESPOOL).length / 2).map(value => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase())
 
 export enum SEARCHPOOL {
-    SYSTEM
+    PROJECT,
+    SYSTEM,
+    HYBRID
 }
+export const searchPool = Object.keys(SEARCHPOOL).slice(Object.keys(SEARCHPOOL).length / 2).map(value => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase())
 
 export interface Settings {
-    getMissingFilesFrom: MISSINGFILESPOOL
-    searchMatchingFilesIn: SEARCHPOOL
-    relinkMethod: RELINKMODE
-    searchPath: string
-    ignoreFileExtensions: boolean
-    perfectMatch: boolean
-    showHelpTips: boolean
+    missingFilesPool?: MISSINGFILESPOOL
+    searchPool?: SEARCHPOOL
+    relinkMethod?: RELINKMETHOD
+    searchPath?: string
+    ignoreFileExtensions?: boolean
+    perfectMatch?: boolean
+    showHelpTips?: boolean
 }
