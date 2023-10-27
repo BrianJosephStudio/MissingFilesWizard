fd5155322ee7b0a36ab79a068a5ef5932d68d06c
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-27 11:04:55 +0200

    Finished dropdown component

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,13 +1,44 @@
 <template>
-    <select :id="Id" :name="Name">
-        <option v-for="option of Options" :value="option">{{ option }}</option>
+    <select class="select" :id="Id" :name="Name" @change="event => dropdownListener(event)">
+        <option class="option" v-for="option of Options" :value="option">{{ option }}</option>
     </select>
 </template>
 <script setup lang="ts">
+import { Settings, MISSINGFILESPOOL, missingFilesPool, SEARCHPOOL, searchPool, RELINKMETHOD, relinkMethod } from "../../types/Settings.d"
+import AppSettings from "@classes/AppSettings.class";
+
+const appSettings = new AppSettings()
 
 const props = defineProps({
     Id: String,
     Name: String,
-    Options: Array
+    Options: Array,
 })
+
+const dropdownListener = async (event: Event): Promise<void> => {
+    const target = event.currentTarget as HTMLSelectElement
+    const value = target.selectedIndex
+    appSettings.changeSetting({ [target.name]: value })
+}
 </script>
+
+<style lang="scss">
+.select {
+    // font-family: 'Comfortaa', sans-serif;
+    font-family: 'Quicksand', sans-serif;
+    text-align: center;
+    background-color: hsl(158, 46%, 57%);
+    // border-radius: 0.3rem;
+    border: none;
+    outline: none;
+    font-size: 1rem;
+    cursor: pointer;
+
+    .option {
+
+        &:hover {
+            background-color: hsl(330, 45%, 72%);
+        }
+    }
+}
+</style>
