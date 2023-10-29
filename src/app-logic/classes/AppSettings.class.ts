b694f7f6a8db744ec7244662c4a4a3adafa02948
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-29 19:13:20 +0100

    finished AppSettings class

--- src/app-logic/classes/AppSettings.class.ts
+++ src/app-logic/classes/AppSettings.class.ts
@@ -1,28 +1,43 @@
-// import { readFile } from "fs/promises"
+import { readFile, writeFile, mkdir } from "fs/promises"
 import config from "../../../config"
-import { Settings, MISSINGFILESPOOL, SEARCHPOOL, RELINKMETHOD } from "../../..//types/Settings.d"
+import { Settings } from "../../..//types/Settings.d"
 export default class AppSettings {
-    constructor() {
+    public readonly settingsFile: string
+    public readonly settingsFolder: string
 
+    constructor() {
+        this.settingsFile = `${config.settingsPath}/${config.settingsFileName}`
+        this.settingsFolder = `${config.settingsPath}`
     }
 
     public async changeSetting(settings: Settings): Promise<Settings> {
-        console.log(settings)
-        return settings
+        const currentSettings = await this.fetchSettings()
+        const newSettings: Settings = {
+            ...currentSettings,
+            ...settings
+        }
+        await writeFile(this.settingsFile, JSON.stringify(newSettings))
+        return newSettings
     }
 
     public async fetchSettings(): Promise<Settings> {
-        // const settingsString = await readFile(config.settingsPath, { encoding: "utf-8" })
-        // const settings = JSON.parse(settingsString) as Settings
-        // return settings
-
-        return config.defaultSettings
-
-    }
-    private async getHomeDir(): Promise<string> {
-        return "HomeDirTest"
-    }
-    public async getProjectDir(): Promise<string> {
-        return "ProjectDirTest"
+        let settingsString: string = ""
+        try {
+            settingsString = await readFile(this.settingsFile, { encoding: "utf-8" })
+        } catch (e: any) {
+            if (e.code === "ENOENT") {
+                await mkdir(this.settingsFolder)
+                await writeFile(this.settingsFile, JSON.stringify(config.defaultSettings))
+                settingsString = await readFile(this.settingsFile, { encoding: "utf-8" })
+                if (!settingsString) {
+                    alert("Error trying to write down Settings file")
+                }
+            } else {
+                alert("error not found")
+            }
+        } finally {
+            const settings = JSON.parse(settingsString) as Settings
+            return settings
+        }
     }
 }
