e8c594baec149e5aa9615edb5ac36896b54a3c12
Brian Joseph <brianjosephstudio@gmail.com>
2023-11-05 16:16:21 +0100

    Imported os lib, pre-processed uri input, fixed bug

--- src/app-logic/classes/FileSystemSearch.class.ts
+++ src/app-logic/classes/FileSystemSearch.class.ts
@@ -6,6 +6,8 @@ import { SearchResults } from "@classes/SearchResults.class";
 import { readdir, stat } from "fs/promises";//-- Production Import
 // import path from "@mocks/path-mock" //!DEBUGMODE
 import path from "path" //-- Production Import
+// import * as os from "@mocks/os-mock"// !DEBUGMODE
+import os from "os" //-- Production Import
 
 export class FileSystemSearch {
     constructor() {
@@ -23,16 +25,19 @@ export class FileSystemSearch {
             return
         }
 
+        targetFolder.uri = targetFolder.uri.replace(/^~/, os.homedir())
+
         const files = await readdir(targetFolder.uri)
 
         for await (const fsFile of files) {
-            const filePath: SystemFile = { name: path.join(targetFolder.uri, fsFile) }
-            const stats = await stat(filePath.name)
+            const filePath: SystemFile = { name: fsFile, uri: path.join(targetFolder.uri, fsFile) }
+            const stats = await stat(filePath.uri!)
 
             if (stats.isDirectory()) {
                 this.search(filePath, targetFolder, currentDepth + 1, maxDepth, results)
             } else if (fsFile === targetFile.name) {
                 results.add(filePath)
+                return //! 'return' limits 'Results' to only one match, if you want to find more potential matches; change to 'break'
             }
         }
     }
